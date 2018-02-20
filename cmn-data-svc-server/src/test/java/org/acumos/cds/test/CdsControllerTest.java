1250d5831bcf68693a7bcbd93cf2c569d1275d4f
Christopher Lott (cl778h) <clott@research.att.com>
2018-02-20 18:40:28 -0500

    Add missing delete-user invocation
    Role and function test left a user row behind.
    
    Change-Id: Id7f46118ea052c5dae0c19a37b8c9e068ee7fd4b
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>

--- cmn-data-svc-server/src/test/java/org/acumos/cds/test/CdsControllerTest.java
+++ cmn-data-svc-server/src/test/java/org/acumos/cds/test/CdsControllerTest.java
@@ -325,7 +325,7 @@ public class CdsControllerTest {
 			cu.setLoginHash(loginPass);
 			final String firstName = "test_" + unique;
 			cu.setFirstName(firstName);
-			final String lastName = "test-last-name";
+			final String lastName = "name create-sol-arts";
 			cu.setLastName(lastName);
 			cu.setActive(true);
 			cu.setLoginPassExpire(new Date());
@@ -835,7 +835,7 @@ public class CdsControllerTest {
 			cu.setLoginHash(loginPass);
 			final String firstName = "test_" + unique;
 			cu.setFirstName(firstName);
-			final String lastName = "test-last-name";
+			final String lastName = "test-role-fn";
 			cu.setLastName(lastName);
 			cu.setActive(true);
 			cu.setLoginPassExpire(new Date());
@@ -946,6 +946,7 @@ public class CdsControllerTest {
 			client.deleteRole(cr.getRoleId());
 			logger.info("Deleting role 2");
 			client.deleteRole(cr2.getRoleId());
+			client.deleteUser(cu.getUserId());
 		} catch (HttpClientErrorException ex) {
 			logger.error("Client reported error; body is {}", ex.getResponseBodyAsString());
 			throw ex;
