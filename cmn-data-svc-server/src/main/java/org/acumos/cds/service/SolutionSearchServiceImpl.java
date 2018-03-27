245a91d209f0ec9a44e68bd602885eef75f99a05
Lott, Christopher (cl778h) <cl778h@att.com>
2018-03-27 15:44:07 -0400

    Refactor search methods 1.15 nr 2
    Revise all methods to use three-step method (count, get IDs,
    get objects) after previous impl failed tests on real data.
    
    Change-Id: Ida337b51ba7f2a517464f2a7851e28e12068b982
    Issue-ID: ACUMOS-529
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- cmn-data-svc-server/src/main/java/org/acumos/cds/service/SolutionSearchServiceImpl.java
+++ cmn-data-svc-server/src/main/java/org/acumos/cds/service/SolutionSearchServiceImpl.java
@@ -32,7 +32,6 @@ import org.acumos.cds.domain.MLPSolutionFOM;
 import org.acumos.cds.util.EELFLoggerDelegate;
 import org.hibernate.AssertionFailure;
 import org.hibernate.Criteria;
-import org.hibernate.FetchMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Disjunction;
@@ -51,8 +50,9 @@ import org.springframework.stereotype.Service;
  * These two aspects must be observed to get pagination working as expected:
  * <OL>
  * <LI>For all to-many mappings, force use of separate select instead of left
- * outer join. This is far less efficient due to repeated trips to the database.
- * </LI>
+ * outer join. This is far less efficient due to repeated trips to the database,
+ * and becomes impossible if you must check properties on mapped (i.e., not the
+ * root) entities.</LI>
  * <LI>Specify an unambiguous ordering. This at least is cheap, just add
  * order-by the ID field.</LI>
  * </OL>
@@ -79,63 +79,64 @@ public class SolutionSearchServiceImpl extends AbstractSearchServiceImpl impleme
 	private final String artAlias = "arts";
 	private final String ownerAlias = "ownr";
 	private final String tagAlias = "tag";
+	private final String solutionId = "solutionId";
 
-	@SuppressWarnings("unchecked")
 	@Override
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public Page<MLPSolution> findSolutions(Map<String, ? extends Object> queryParameters, boolean isOr,
 			Pageable pageable) {
+
+		// Step 0: build the query
 		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(MLPSolution.class);
 		super.buildCriteria(criteria, queryParameters, isOr);
 
-		// Adjust fetch mode on tags to block Hibernate from using left outer join,
-		// which builds a cross product that contains duplicate rows.
-		// This is a horrid violation of information hiding.
-		criteria.setFetchMode("tags", FetchMode.SELECT);
-
-		// Count the total rows
-		criteria.setProjection(Projections.rowCount());
+		// Step 1: count the distinct IDs
+		criteria.setProjection(Projections.countDistinct(solutionId));
 		Long count = (Long) criteria.uniqueResult();
 		if (count == 0)
 			return new PageImpl<>(new ArrayList<>(), pageable, count);
 
-		// Reset the count criteria
-		criteria.setProjection(null);
-		// This should not do any harm; had problems elsewhere without
-		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		// Step 2: fetch the list of IDs, with page and sort criteria
+		criteria.setProjection(Projections.distinct(Projections.property(solutionId)));
 		// Add pagination and sort
 		super.applyPageableCriteria(criteria, pageable);
 		// Fallback order on a unique field. Without this the pagination
 		// yields odd results; e.g., request 10 items but only get 8.
-		criteria.addOrder(Order.asc("solutionId"));
+		criteria.addOrder(Order.asc(solutionId));
+		List uniqueSubList = criteria.list();
 
-		// Get a page of results and send it back with the total available
-		List<MLPSolution> items = criteria.list();
+		// Step 3: using fresh criteria, fetch objects by ID
+		criteria = sessionFactory.getCurrentSession().createCriteria(MLPSolution.class);
+		criteria.add(Restrictions.in(solutionId, uniqueSubList));
+		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		super.applySortCriteria(criteria, pageable);
+		criteria.addOrder(Order.asc(solutionId));
+		List items = criteria.list();
+		// This detects programmer errors
+		if (items.isEmpty())
+			throw new AssertionFailure("findPortalSolutions: unexpected empty result");
+
 		logger.debug(EELFLoggerDelegate.debugLogger, "findSolutions: result size={}", items.size());
 		return new PageImpl<>(items, pageable, count);
 	}
 
 	/*
-	 * Searches using the full object model, then converts result to plain.
+	 * Three-step algorithm as sketched out on StackOverflow
 	 *
 	 * This implementation is awkward due to LIKE queries on certain fields
 	 */
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public Page<MLPSolution> findPortalSolutions(String[] nameKeywords, String[] descKeywords, boolean active,
 			String[] ownerIds, String[] modelTypeCode, String[] accessTypeCode, String[] validationStatusCode,
 			String[] tags, Pageable pageable) {
 
+		// Step 0: build the query on a full-object mapped version
 		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(MLPSolutionFOM.class);
-
-		// Aliases for subclasses
 		criteria.createAlias("revisions", revAlias);
 		criteria.createAlias(revAlias + ".artifacts", artAlias);
 		criteria.createAlias("owner", ownerAlias);
 		criteria.createAlias("tags", tagAlias);
-
-		// FOM domain classes use LAZY fetch mode for *-to-many fields
-		// so there is no need to set the fetch mode here.
-
 		// Attributes on the solution
 		criteria.add(Restrictions.eq("active", active));
 		if (nameKeywords != null && nameKeywords.length > 0)
@@ -154,58 +155,54 @@ public class SolutionSearchServiceImpl extends AbstractSearchServiceImpl impleme
 		if (tags != null && tags.length > 0)
 			criteria.add(Restrictions.in(tagAlias + ".tag", tags));
 
-		// Request count of rows
-		criteria.setProjection(Projections.rowCount());
+		// Step 1: count the distinct IDs
+		criteria.setProjection(Projections.countDistinct(solutionId));
 		Long count = (Long) criteria.uniqueResult();
 		if (count == 0)
 			return new PageImpl<>(new ArrayList<>(), pageable, count);
 
-		// Remove the count projection
-		criteria.setProjection(null);
-		// This should not do any harm; had problems elsewhere without
-		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		// Step 2: fetch the list of IDs, with page and sort criteria
+		criteria.setProjection(Projections.distinct(Projections.property(solutionId)));
 		// Add pagination and sort
 		super.applyPageableCriteria(criteria, pageable);
+		// Fallback order on a unique field. Without this the pagination
+		// yields odd results; e.g., request 10 items but only get 8.
+		criteria.addOrder(Order.asc(solutionId));
+		List uniqueSubList = criteria.list();
 
-		// Get a page of results
-		List<MLPSolutionFOM> items = criteria.list();
+		// Step 3: using fresh criteria on non-FOM class, fetch objects by ID
+		criteria = sessionFactory.getCurrentSession().createCriteria(MLPSolution.class);
+		criteria.add(Restrictions.in(solutionId, uniqueSubList));
+		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		super.applySortCriteria(criteria, pageable);
+		criteria.addOrder(Order.asc(solutionId));
+		List items = criteria.list();
 		// This detects programmer errors
 		if (items.isEmpty())
 			throw new AssertionFailure("findPortalSolutions: unexpected empty result");
 
-		// Convert from FOM to plain
-		List<MLPSolution> solutions = new ArrayList<>();
-		for (Object item : items)
-			if (item instanceof MLPSolutionFOM)
-				solutions.add(((MLPSolutionFOM) item).toMLPSolution());
-			else
-				throw new AssertionFailure("findPortalSolutions: unexpected type: {} " + item.getClass().getName());
-
-		logger.debug(EELFLoggerDelegate.debugLogger, "findPortalSolutions: result size={}", solutions.size());
-		return new PageImpl<>(solutions, pageable, count);
+		logger.debug(EELFLoggerDelegate.debugLogger, "findPortalSolutions: result size={}", items.size());
+		return new PageImpl<>(items, pageable, count);
 	}
 
 	/*
-	 * Searches using the full object model, then converts result to plain.
+	 * Three-step algorithm as sketched out on StackOverflow
 	 */
 	@Override
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public Page<MLPSolution> findSolutionsByModifiedDate(boolean active, String[] accessTypeCode,
 			String[] validationStatusCode, Date date, Pageable pageable) {
 
+		// Step 0: build the query using FOM to access child attributes
 		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(MLPSolutionFOM.class);
 		criteria.createAlias("revisions", revAlias);
 		criteria.createAlias(revAlias + ".artifacts", artAlias);
-
-		// FOM domain classes use LAZY fetch mode for *-to-many fields
-		// so there is no need to set the fetch mode here.
-
+		criteria.createAlias(revAlias + ".solution", "sol");
 		criteria.add(Restrictions.eq("active", active));
 		if (accessTypeCode != null && accessTypeCode.length > 0)
 			criteria.add(Restrictions.in(revAlias + ".accessTypeCode", accessTypeCode));
 		if (validationStatusCode != null && validationStatusCode.length > 0)
 			criteria.add(Restrictions.in(revAlias + ".validationStatusCode", validationStatusCode));
-
 		// Construct a disjunction to find any updated item.
 		// Unfortunately this requires hard-coded field names
 		Criterion solModified = Restrictions.ge("modified", date);
@@ -217,39 +214,31 @@ public class SolutionSearchServiceImpl extends AbstractSearchServiceImpl impleme
 		itemModifiedAfter.add(artModified);
 		criteria.add(itemModifiedAfter);
 
-		// Request count of rows
-		criteria.setProjection(Projections.rowCount());
+		// Step 1: count the distinct IDs
+		criteria.setProjection(Projections.countDistinct(solutionId));
 		Long count = (Long) criteria.uniqueResult();
 		if (count == 0)
 			return new PageImpl<>(new ArrayList<>(), pageable, count);
 
-		// Remove the count projection
-		criteria.setProjection(null);
-		// Without this the result becomes a list of (solution, revision, artifact)!
-		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-		// Add pagination and sort
+		// Step 2: fetch the list of IDs, with page and sort criteria
+		criteria.setProjection(Projections.distinct(Projections.property(solutionId)));
 		super.applyPageableCriteria(criteria, pageable);
-		// Fallback order on a unique field. Without this the pagination
-		// yields odd results; e.g., request 10 items but only get 8.
-		criteria.addOrder(Order.asc("solutionId"));
+		// Ensure unambiguous order with ID field
+		criteria.addOrder(Order.asc(solutionId));
+		List uniqueSubList = criteria.list();
 
-		// Get a page of results
+		// Step 3: using fresh criteria on non-FOM class, fetch objects by ID
+		criteria = sessionFactory.getCurrentSession().createCriteria(MLPSolution.class);
+		criteria.add(Restrictions.in(solutionId, uniqueSubList));
+		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		super.applySortCriteria(criteria, pageable);
+		criteria.addOrder(Order.asc(solutionId));
 		List items = criteria.list();
 		// This detects programmer errors
 		if (items.isEmpty())
-			throw new AssertionFailure("findSolutionsByModifiedDate: unexpected empty result");
+			throw new AssertionFailure("findPortalSolutions: unexpected empty result");
 
-		// Convert from FOM to plain
-		List<MLPSolution> solutions = new ArrayList<>();
-		for (Object item : items)
-			if (item instanceof MLPSolutionFOM)
-				solutions.add(((MLPSolutionFOM) item).toMLPSolution());
-			else
-				throw new AssertionFailure(
-						"findSolutionsByModifiedDate: unexpected type: " + item.getClass().getName());
-
-		logger.debug(EELFLoggerDelegate.debugLogger, "findSolutionsByModifiedDate: result size={}", solutions.size());
-		return new PageImpl<>(solutions, pageable, count);
+		return new PageImpl<>(items, pageable, count);
 	}
 
 }
