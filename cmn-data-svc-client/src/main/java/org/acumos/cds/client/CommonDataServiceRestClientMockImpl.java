5c7e2a0db0f6e17966fefdb3e4aabf346b825ffe
Lott, Christopher (cl778h) <cl778h@att.com>
2018-11-27 16:06:22 -0500

    Factor picture out of MLPSolution
    Add new methods to get and save a solution picture.
    
    Change-Id: Ic2787bcfb08ee27b29d4b4dd163f36e266d3a90b
    Issue-ID: ACUMOS-2088
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- cmn-data-svc-client/src/main/java/org/acumos/cds/client/CommonDataServiceRestClientMockImpl.java
+++ cmn-data-svc-client/src/main/java/org/acumos/cds/client/CommonDataServiceRestClientMockImpl.java
@@ -224,6 +224,7 @@ public class CommonDataServiceRestClientMockImpl implements ICommonDataServiceRe
 	private MLPPublishRequest publishRequest;
 	private RestPageResponse<MLPSolution> restrictedSolutions;
 	private long userNotificationCount;
+	private byte[] solutionImage;
 
 	/**
 	 * No-argument constructor.
@@ -1895,6 +1896,16 @@ public class CommonDataServiceRestClientMockImpl implements ICommonDataServiceRe
 	@Override
 	public void dropUserTag(String userId, String tag) {
 		// what to mock?
+	}
+
+	@Override
+	public byte[] getSolutionPicture(String solutionId) {
+		return this.solutionImage;
+	}
+
+	@Override
+	public void saveSolutionPicture(String solutionId, byte[] image) {
+		this.solutionImage = image;
 	}
 
 }
