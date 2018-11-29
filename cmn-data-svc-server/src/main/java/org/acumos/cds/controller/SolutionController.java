8c57b8d0fab7b02cd1bec05fcc8bb4bbf70f0029
Lott, Christopher (cl778h) <cl778h@att.com>
2018-11-29 14:06:33 -0500

    Add method to get peer subscription count
    Cleaned up some minor non-functional botches of
    Long/long usage in controller and repository methods.
    
    Change-Id: Ie78ede065f1eeeca42750fa56aced3913cca1a92
    Issue-ID: ACUMOS-2006
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- cmn-data-svc-server/src/main/java/org/acumos/cds/controller/SolutionController.java
+++ cmn-data-svc-server/src/main/java/org/acumos/cds/controller/SolutionController.java
@@ -144,7 +144,7 @@ public class SolutionController extends AbstractController {
 	 *                       Solution ID
 	 */
 	private void updateSolutionDownloadStats(String solutionId) {
-		Long count = solutionDownloadRepository.getSolutionDownloadCount(solutionId);
+		Long count = solutionDownloadRepository.countSolutionDownloads(solutionId);
 		if (count != null) {
 			MLPSolutionWeb stats = solutionWebRepository.findOne(solutionId);
 			stats.setDownloadCount(count);
@@ -163,9 +163,9 @@ public class SolutionController extends AbstractController {
 	 *                       Solution ID
 	 */
 	private void updateSolutionRatingStats(String solutionId) {
-		Long count = solutionRatingRepository.getSolutionRatingCount(solutionId);
+		long count = solutionRatingRepository.countSolutionRatings(solutionId);
 		Double avg = solutionRatingRepository.getSolutionRatingAverage(solutionId);
-		if (count != null && avg != null) {
+		if (count > 0 && avg != null) {
 			MLPSolutionWeb stats = solutionWebRepository.findOne(solutionId);
 			stats.setRatingCount(count);
 			stats.setRatingAverageTenths(Math.round(10 * avg));
@@ -177,7 +177,7 @@ public class SolutionController extends AbstractController {
 	@RequestMapping(value = CCDSConstants.COUNT_PATH, method = RequestMethod.GET)
 	public CountTransport getSolutionCount() {
 		logger.debug("getSolutionCount");
-		Long count = solutionRepository.count();
+		long count = solutionRepository.count();
 		return new CountTransport(count);
 	}
 
