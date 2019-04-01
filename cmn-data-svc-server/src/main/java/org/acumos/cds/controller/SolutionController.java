183d5f961d059213d85409241b1600820939ea3f
Lott, Christopher (cl778h) <cl778h@att.com>
2019-04-01 15:44:57 -0400

    Drop access type code from solution revision
    Change-Id: Ic5a5752784edafa237a4a9dbeb8d101068852862
    Issue-Id: ACUMOS-2569, ACUMOS-2683
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- cmn-data-svc-server/src/main/java/org/acumos/cds/controller/SolutionController.java
+++ cmn-data-svc-server/src/main/java/org/acumos/cds/controller/SolutionController.java
@@ -260,8 +260,6 @@ public class SolutionController extends AbstractController {
 	public Object findPortalSolutions( //
 			@ApiParam(value = "Active Y/N", required = true) //
 			@RequestParam(name = CCDSConstants.SEARCH_ACTIVE, required = true) boolean active, //
-			@ApiParam(value = "Access type codes", allowMultiple = true) //
-			@RequestParam(name = CCDSConstants.SEARCH_ACCESS_TYPES, required = false) String[] accTypeCodes, //
 			@ApiParam(value = "Model type codes", allowMultiple = true) //
 			@RequestParam(name = CCDSConstants.SEARCH_MODEL_TYPES, required = false) String[] modelTypeCodes, //
 			@ApiParam(value = "User IDs", allowMultiple = true) //
@@ -279,8 +277,8 @@ public class SolutionController extends AbstractController {
 			Pageable pageRequest, HttpServletResponse response) {
 		logger.debug("findPortalSolutions: active {} nameKws {}", active, nameKws);
 		try {
-			return solutionSearchService.findPortalSolutions(nameKws, descKws, active, userIds, modelTypeCodes,
-					accTypeCodes, tags, authKws, pubKws, pageRequest);
+			return solutionSearchService.findPortalSolutions(nameKws, descKws, active, userIds, modelTypeCodes, tags,
+					authKws, pubKws, pageRequest);
 		} catch (Exception ex) {
 			logger.error("findPortalSolutions failed", ex);
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -299,8 +297,6 @@ public class SolutionController extends AbstractController {
 	public Object findPortalSolutionsByKwAndTags( //
 			@ApiParam(value = "Active Y/N", required = true) //
 			@RequestParam(name = CCDSConstants.SEARCH_ACTIVE, required = true) boolean active, //
-			@ApiParam(value = "Access type codes", allowMultiple = true) //
-			@RequestParam(name = CCDSConstants.SEARCH_ACCESS_TYPES, required = false) String[] accTypeCodes, //
 			@ApiParam(value = "Model type codes", allowMultiple = true) //
 			@RequestParam(name = CCDSConstants.SEARCH_MODEL_TYPES, required = false) String[] modelTypeCodes, //
 			@ApiParam(value = "Key words", allowMultiple = true) //
@@ -316,8 +312,8 @@ public class SolutionController extends AbstractController {
 			Pageable pageRequest, HttpServletResponse response) {
 		logger.debug("findPortalSolutionsByKwAndTags: active {} kw {}", active, kws);
 		try {
-			return solutionSearchService.findPortalSolutionsByKwAndTags(kws, active, userIds, modelTypeCodes,
-					accTypeCodes, allTags, anyTags, catalogIds, pageRequest);
+			return solutionSearchService.findPortalSolutionsByKwAndTags(kws, active, userIds, modelTypeCodes, allTags,
+					anyTags, catalogIds, pageRequest);
 		} catch (Exception ex) {
 			logger.error("findPortalSolutionsByKwAndTags failed", ex);
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -532,8 +528,6 @@ public class SolutionController extends AbstractController {
 		}
 		try {
 			// Validate enum codes
-			if (revision.getAccessTypeCode() != null)
-				super.validateCode(revision.getAccessTypeCode(), CodeNameType.ACCESS_TYPE);
 			if (revision.getVerifiedLicense() != null)
 				super.validateCode(revision.getVerifiedLicense(), CodeNameType.VERIFIED_LICENSE);
 			if (revision.getVerifiedVulnerability() != null)
@@ -580,8 +574,6 @@ public class SolutionController extends AbstractController {
 		}
 		try {
 			// Validate enum codes
-			if (revision.getAccessTypeCode() != null)
-				super.validateCode(revision.getAccessTypeCode(), CodeNameType.ACCESS_TYPE);
 			if (revision.getVerifiedLicense() != null)
 				super.validateCode(revision.getVerifiedLicense(), CodeNameType.VERIFIED_LICENSE);
 			if (revision.getVerifiedVulnerability() != null)
